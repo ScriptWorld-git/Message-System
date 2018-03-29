990adcb45f9748c1a21ca85dd8b952c273e4585c
Felix Breuer <f.breuer94@gmail.com>
2018-03-29 13:00:59 +0200

    Initial commit

---
+++ MessageSystem/Message.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MessageSystem
+{
+    internal class Message : IComparable<Message>
+    {
+        private int group;
+        private DateTime timeStamp;
+        private object message;
+
+        public Message(object message, int group = -1)
+        {
+            this.group = group;
+            this.timeStamp = DateTime.Now;
+            this.message = message;
+        }
+
+        public string getTimeStampString()
+        {
+            return timeStamp.ToString("dd/MM-HH:mm:ss:ffff");
+        }
+
+        public override string ToString()
+        {
+            return getTimeStampString() + " | "
+        }
+
+        public int CompareTo(Message other)
+        {
+            return DateTime.Compare(this.timeStamp, other.timeStamp);
+        }
+    }
+}
---
+++ MessageSystem/MsgSystem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MessageSystem
+{
+    public class MsgSystem
+    {
+        private List<Message> messages = new List<Message>();
+        private Dictionary<int, string> groups = new Dictionary<int, string>();
+
+
+        public MsgSystem()
+        {
+            groups.Add(-1, "MSG");
+        }
+
+        public void addGroup(int ident, string name)
+        {
+            string temp;
+            bool found = groups.TryGetValue(ident, out temp);
+
+            if (!found)
+            {
+                groups.Add(ident, name);
+            }
+        }
+
+        public void log(object message)
+        {
+            messages.Add(new Message(message, -1));
+        }
+
+        public void log(object message, int group)
+        {
+            messages.Add(new Message(message, group));
+        }
+    }
+}
